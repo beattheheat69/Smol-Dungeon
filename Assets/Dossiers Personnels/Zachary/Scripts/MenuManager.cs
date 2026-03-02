2bd1d3d0f534fd703a05bb23ba33cdbe94d4ff7a
WhispyWomp <125895524+ZachaBoss85@users.noreply.github.com>
2026-03-02 16:59:52 -0500

    More Assets
    - Added a custom cursor
    - Added more functionality to the settings menu
    - Added/updated some assets

--- Assets/Dossiers Personnels/Zachary/Scripts/MenuManager.cs
+++ Assets/Dossiers Personnels/Zachary/Scripts/MenuManager.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
 
     public Button continueButton;
-    [SerializeField] string newGameSceneString = "ConstructionMenu";
     [SerializeField] bool isContinueAvailable;
+
+    [SerializeField] GameObject pauseWindowPrefab;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -28,17 +30,29 @@ public class MainMenu : MonoBehaviour
         {
             continueButton.interactable = true;
         }
+
+        //FOR DEBUG PURPOSES (Test Pause Menu)
+
+        if (UnityEngine.InputSystem.Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            PopUpWindow(pauseWindowPrefab);
+        }
     }
 
     public void LoadNewGame()
     {
         //Load the scene with the Dungeon Shaping Screen
-        SceneManager.LoadScene(newGameSceneString);
+        SceneManager.LoadScene("Dungeon Shape Screen");
     }
 
     public void LoadResumedGame()
     {
         //We'll get there when we get there
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void PopUpWindow(GameObject window)
@@ -48,7 +62,18 @@ public class MainMenu : MonoBehaviour
 
         //Make the window appear
         window.SetActive(true);
+    }
 
+    public void SetFullScreen (bool fullscreen)
+    {
+        if (fullscreen)
+        {
+            Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+        }
+        else
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+        }
     }
 
     public void QuitGame()
