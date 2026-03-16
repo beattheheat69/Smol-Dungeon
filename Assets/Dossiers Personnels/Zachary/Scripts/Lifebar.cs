b45123d63777109eb6642e9599bc3458c5802086
WhispyWomp <125895524+ZachaBoss85@users.noreply.github.com>
2026-03-16 11:23:20 -0400

    Health Bar
    - Made some progress on the health bar prefab

--- Assets/Dossiers Personnels/Zachary/Scripts/Lifebar.cs
+++ Assets/Dossiers Personnels/Zachary/Scripts/Lifebar.cs
@@ -1,24 +1,28 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class Lifebar : MonoBehaviour
 {
-    [Header("Position")]
-    public float verticalOffset = 0f;
+    public Slider slider;
+    public Image fill;
 
     [Header("Health Values")]
     public float maxHealth;
     public float currentHealth;
 
-    public Slider slider;
+    [Header("Color")]
+    public Gradient gradient;
+
+    [Header("Animation Values")]
+    public float animSpeed = 1f;
+    bool isAnimating = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Make the player unable to change the value of the slider
-        slider.interactable = false;
-
         //Set the max value of the Slider
         SetMaxHealth(maxHealth);
     }
@@ -26,9 +30,9 @@ public class Lifebar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Make the health bar appear over the entity's head whenever it's attacked?
+        //Bound the health between 0 and the max health
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
 
-        //Update the current health (FOR DEBUG PURPOSES)
         SetHealth(currentHealth);
     }
 
@@ -38,9 +42,13 @@ public class Lifebar : MonoBehaviour
         slider.value = maxHealth;
     }
 
-    public void SetHealth (float health)
+    public void SetHealth(float health)
     {
+        //Set the value of the slider to the current health
         slider.value = health;
+
+        //Set the color of the slider according to the remaining health
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 
     public void SetAsActive(bool active)
