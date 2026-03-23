0b5db8105137d0d1487006c66545782b09c68079
WhispyWomp <125895524+ZachaBoss85@users.noreply.github.com>
2026-03-23 10:11:21 -0400

    Main Menu Fix
    Changed the Canvas from Screen Space Overlay to Screen Space Camera

--- Assets/Dossiers Personnels/Alex/Scripts/RoomEntityLimit.cs
+++ Assets/Dossiers Personnels/Alex/Scripts/RoomEntityLimit.cs
@@ -1,8 +1,14 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RoomEntityLimit : MonoBehaviour
 {
+    [Header("Integer Limits")]
     public int monsterLimit = 3;
     public int trapLimit = 3;
 	public int totalLimit = 4;
+
 }
