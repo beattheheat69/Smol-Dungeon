478564b6fcb017f140e512f303f10dbe61a67a8b
alex <alextrubiano@gmail.com>
2026-03-23 23:35:35 -0400

    HeroAnim
    Added hero walk
    Sword
    attack anim
    added heroanim script to manage animations
    bug: walk direction not update unless hero has a target
    added sword trigger with 'Sword' tag

---
+++ Assets/Dossiers Personnels/Alex/Scripts/HeroAnimation.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class HeroAnimation : MonoBehaviour
+{
+    HeroAI heroAI;
+	Animator heroAnim;
+	public Animator swordAnim;
+	Vector2 direction;
+
+	private void Start()
+	{
+		heroAI = GetComponent<HeroAI>();
+		heroAnim = GetComponent<Animator>();
+		swordAnim = GameObject.Find("Sword").GetComponent<Animator>();
+	}
+
+	private void FixedUpdate()
+	{
+		direction = heroAI.lastMoveDirection; //Bug: Grabs 0 in entrance, find correct var to get movement
+
+		if (direction != Vector2.zero)
+		{
+			heroAnim.SetBool("isWalking", true);
+		}
+		else
+		{
+			heroAnim.SetBool("isWalking", false);
+		}
+
+		heroAnim.SetFloat("dirX", direction.x);
+		heroAnim.SetFloat("dirY", direction.y);
+
+		swordAnim.SetFloat("dirX", direction.x);
+		swordAnim.SetFloat("dirY", direction.y);
+	}
+
+	public void IsAttacking()
+	{
+		heroAnim.SetTrigger("Attack");
+		swordAnim.SetTrigger("Slash");
+	}
+}
